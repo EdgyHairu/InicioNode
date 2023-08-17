9aa71556b53256a5e3923f3dbbd8ed0b2e7b542c
EdgyHairu <jairarmando2012@outlook.com>
2023-08-17 09:03:01 -0600

    Usamos cowsay

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const { frutas, money } = require('./frutas')
+const cowsay = require('cowsay')
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+
+/*const { frutas, money } = require('./frutas')
 
 frutas.forEach(fruta => {
     console.log('La fruta es: ', fruta)
 })
-console.log('Dinero: ', money)
+console.log('Dinero: ', money)*/
